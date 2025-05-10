4a95104d36492229a714c7aef4fd32fb00e9e222
Sam Brook <brookyy@gmail.com>
2025-05-10 19:00:45 +1200

    testing version build command

--- v2/vite.config.js
+++ v2/vite.config.js
@@ -4,13 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }) => {
+  const versionName = process.env.VERSION_NAME || 'dist'; // Default to 'dist' if no version name is provided
+  const outDir = command === 'build' ? `../${versionName}` : 'dist';
+  const base = command === 'build' ? `/Soccer-Team-Manager/${versionName}/` : '/';
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: base,
+    build: {
+      outDir: outDir,
     }
-  }
+  };
 })
